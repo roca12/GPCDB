8d5c39b900a87201760e48e0134637bee5438ced
roca12 <roca230313@gmail.com>
2020-12-06 18:21:30 -0500

    no message

---
+++ src/java/entities/TemarioFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author roca12
+ */
+@Stateless
+public class TemarioFacade extends AbstractFacade<Temario> {
+
+    @PersistenceContext(unitName = "testPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public TemarioFacade() {
+        super(Temario.class);
+    }
+    
+}
